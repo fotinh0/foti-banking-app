7ac598f19a0347e9ec6830dad4d2703a67ac2eba
Foti Ceci <foti.ceci@gmail.com>
2024-05-07 12:16:54 -0400

    implement page layouts

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,64 @@
+import HeaderBox from "@/components/HeaderBox";
+import RightSideBar from "@/components/RightSideBar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  const loggedIn = {
+    firstName: "Foti",
+    lastName: "C",
+    email: "foti.ceci@gmail.com",
+  };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+      </div>
+
+      <RightSideBar
+        user={loggedIn}
+        transaction={[]}
+        banks={[{ currentBalance: 1450.24 }, { currentBalance: 200.32 }]}
+      />
+    </section>
+    // <section className="home">
+    //   <div className="home-content">
+    //     <header className="home=header">
+    //       <HeaderBox
+    //         type="greeting"
+    //         title="Welcome"
+    //         user={loggedIn?.firstName || "Guest"}
+    //         subtext="Access and manage your account and transcactions efficiently."
+    //       />
+
+    // <TotalBalanceBox
+    //   accounts={[]}
+    //   totalBanks={1}
+    //   totalCurrentBalance={1250.35}
+    // />
+    //     </header>
+    //   </div>
+
+    // <RightSideBar
+    //   user={loggedIn}
+    //   transaction={[]}
+    //   banks={[{ currentBalance: 1450.24 }, { currentBalance: 200.32 }]}
+    // />
+    // </section>
+  );
 };
 
 export default Home;
