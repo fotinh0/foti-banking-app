eb401a67a4f1f1779f8eb98f6049ff35175bec1d
Foti Ceci <foti.ceci@gmail.com>
2024-05-08 15:22:22 -0400

    fix my banks and bank card

--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -2,12 +2,14 @@ import { formatAmount } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import Copy from "./Copy";
 
 const BankCard = ({
   account,
   userName,
   showBalance = true,
 }: CreditCardProps) => {
+  console.log(account);
   return (
     <div className="flex flex-col">
       <Link
@@ -28,7 +30,7 @@ const BankCard = ({
               <h2 className="text-12 font-semibold text-white">●● / ●●</h2>
             </div>
             <p className="text-14 font-semibold tracking-[1.1px] text-white">
-              ●●●● ●●●● ●●●● <span className="text-16">{1234}</span>
+              ●●●● ●●●● ●●●● <span className="text-16">{account?.mask}</span>
             </p>
           </article>
         </div>
@@ -52,6 +54,8 @@ const BankCard = ({
           className="absolute top-0 left-0"
         />
       </Link>
+
+      {showBalance && <Copy title={account?.shareableId} />}
     </div>
   );
 };
